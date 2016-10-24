f5c06bda99232190c4cbd5b27e24e4debe3d3285
Adrian Delgado <k1502820@kcl.ac.uk>
2016-10-24 17:57:43 +0100

    part a.)

--- treenode.h
+++ treenode.h
@@ -15,6 +15,56 @@ using std::pair;
 
 // TODO your code for the TreeNode class goes here:
 
+template<typename T>
+class TreeNode{
+	
+public:
+	T data;
+	unique_ptr<TreeNode<T>> leftChild;
+	unique_ptr<TreeNode<T>> rightChild;
+	TreeNode<T> * parent;
+	
+	TreeNode(T dataIn)
+		:data(dataIn),parent(nullptr){
+	}
+
+	void setLeftChild(TreeNode *child){
+	
+		leftChild.reset(child);
+		leftChild->parent=this;
+		
+	}
+	
+	void setRightChild(TreeNode *child){
+	
+		rightChild.reset(child);
+		rightChild->parent=this;
+		
+	}
+	
+	const ostream & write(ostream & o){
+	
+		if(leftChild){
+		
+			//o<<"HIT"<<endl;
+			leftChild->write(o);
+		
+		}	
+		
+		o<<" "<<data<<" ";
+		
+		if(rightChild){
+			
+			rightChild->write(o);
+			
+		}
+		
+		return o;
+		
+	}
+	
+};
+
 // do not edit below this line
 
 #endif
